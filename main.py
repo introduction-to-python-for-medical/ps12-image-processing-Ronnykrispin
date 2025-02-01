b8c25e74df41bd539f818880d5dd55b04f1341d4
Ronnykrispin <ronnykrispin@gmail.com>
2025-02-01 19:40:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,15 +15,15 @@ def detect_edges (clean_image):
 def convert_to_binary(edges, threshold):
       binary_image= (edges > threshold).astype(np.uint8)
       return binary_image
-def save_binary_image (binary_image, file_name):
+def save_binary_image(binary_image, file_name):
        edge_image= Image.fromarray(binary_image * 255)
-       edge image.save(file_name)
+       edge_image.save(file_name)
 
 image_array = load_image('python.jpg')
 clean_image= suppress_noise(image_array)
 edges= detect_edges(clean_image)
 binary_edges=convert_to_binary(edges, threshold=50)
-save_binary_image (binary_edges, 'my_edges.png'):
+save_binary_image (binary_edges, 'my_edges.png')
 
 plt.imshow(binary_edges, cmap='gray')
 plt.show()
