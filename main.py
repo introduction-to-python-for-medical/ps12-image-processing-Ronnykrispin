5a4ec6bf6b48cd1576427d03a23053bea4a36dd0
Ronnykrispin <ronnykrispin@gmail.com>
2025-02-01 19:23:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,46 +4,27 @@ from skimage.filters import median
 from skimage.morphology import ball
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.ndimage import convolve
 
-# Load the image
-import urllib.request
-import matplotlib.pyplot as plt
-from image_utils import load_image  # מייבאים את הפונקציה מקובץ image_utils.py
+def suppress_noise(image_array):
+    clean_image = median(image_array, ball(3))
+    return clean_image
+def detect_edges (clean_image):
+     edges=edge_detection(clean_image)
+     return edges
+def convert_to_binary(edges, threshold):
+      binary_image= (edges > threshold).astype(np.uint8)
+      return binary_image
+def save_binary_image (binary_image, file_name):
+       edge_image= Image.fromarray(binary_image * 255)
+       edge image.save(file_name)
 
-url = "https://raw.githubusercontent.com/introduction-to-python-for-medical/ps12-image-processing-Ronnykrispin/refs/heads/main/1024px-Python-logo-notext.svg_.png.webp"  
-file_path = "image.jpg"
+image_array = load_image('python.jpg')
+clean_image= suppress_noise(image_array)
+edges= detect_edges(clean_image)
+binary_edges=convert_to_binary(edges, threshold=50)
+save_binary_image (binary_edges, 'my_edges.png'):
 
-urllib.request.urlretrieve(url, file_path)
-print("✔ התמונה נשמרה בהצלחה:", file_path)
-
-# 3. טעינת התמונה עם הפונקציה מ-image_utils.py
-image_array = load_image(file_path)
-
-# 4. הצגת התמונה
-plt.imshow(image_array)
-plt.axis("off")
+plt.imshow(binary_edges, cmap='gray')
 plt.show()
 
-# 5. בדיקת הצורה של התמונה
-print("📏 גודל התמונה:", image_array.shape)
-
-if image is not None:
-    # Apply median filter for noise reduction
-    clean_image = median(image, ball(3))
-
-    # Perform edge detection
-    edgeMAG = edge_detection(clean_image)
-
-    # Display the histogram to choose a threshold
-    plt.hist(edgeMAG.ravel(), bins=256)
-    plt.show()
-
-    # Choose a threshold value based on the histogram (example: 50)
-    threshold = 50  
-    edge_binary = edgeMAG > threshold
-
-    # Convert the binary array to an image and save it
-    edge_image = Image.fromarray((edge_binary * 255).astype(np.uint8))
-    edge_image.save('my_edges.png')
-    plt.imshow(edge_binary, cmap='gray')
-    plt.show()
